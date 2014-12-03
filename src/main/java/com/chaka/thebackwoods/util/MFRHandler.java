ff9f008cae0355ec1a67e986f1c9ff989cc91a51
coolsquid <snakkeflink@gmail.com>
2014-12-03 23:23:59 +0100

    Update MFRHandler.java

--- src/main/java/com/chaka/thebackwoods/util/MFRHandler.java
+++ src/main/java/com/chaka/thebackwoods/util/MFRHandler.java
@@ -10,7 +10,7 @@ public class MFRHandler {
 		if (event.Name == "Greggy_greg_do_please_kindly_stuff_a_sock_in_it_xxx") {
 			int a = 1;
 			while (a <= 5) {
-				LogHandler.warn("Skyggy_sky_do_please_kindly_shut_up");
+				LogHelper.warn("Skyggy_sky_do_please_kindly_shut_up");
 				a++;
 			}
 		}
